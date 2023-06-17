e33119dd56f94ce7025d6bacd4e522f2c6370488
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-06-17 18:00:10 +0530

    :zap: chore: commonn utilities

---
+++ src/common/utils/formatStringQuery.ts
@@ -0,0 +1,29 @@
+export const formatInputQuery = <T>(filterEle: T) => {
+  let formattedFilterEle = {};
+  if (filterEle['equalTo']) {
+    formattedFilterEle = filterEle['equalTo'];
+  } else if (filterEle['notEqualTo']) {
+    formattedFilterEle['$ne'] = filterEle['notEqualTo'];
+  } else if (filterEle['exists']) {
+    formattedFilterEle['$exists'] = filterEle['exists'];
+  } else if (
+    filterEle['greaterThan'] ||
+    filterEle['greaterThanEqual'] ||
+    filterEle['lessThan'] ||
+    filterEle['lessThanEqual']
+  ) {
+    if (filterEle['greaterThan']) {
+      formattedFilterEle['$gt'] = filterEle['greaterThan'];
+    } else if (filterEle['greaterThanEqual']) {
+      formattedFilterEle['$gte'] = filterEle['greaterThanEqual'];
+    }
+
+    if (filterEle['lessThan']) {
+      formattedFilterEle['$lt'] = filterEle['lessThan'];
+    } else if (filterEle['lessThanEqual']) {
+      formattedFilterEle['$lte'] = filterEle['lessThanEqual'];
+    }
+  }
+
+  return formattedFilterEle;
+};
---
+++ src/common/utils/request-context.ts
@@ -0,0 +1,17 @@
+import { requestContext } from '@fastify/request-context';
+
+declare module '@fastify/request-context' {
+  interface RequestContextData {
+    [key: string]: any;
+  }
+}
+
+export default class RequestContext {
+  static get<T>(key: string): T | null {
+    return requestContext.get(key) || null;
+  }
+
+  static set<T>(key: string, value: T): void {
+    requestContext.set(key, value);
+  }
+}
