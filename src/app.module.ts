ecabeff153f2796ca678772b87c1a1a73c8eeb1b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-12-15 03:17:15 +0530

    :package: chore: updating app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Context } from 'apollo-server-core';
 import { __prod__ } from './constants';
 import { LoggerModule } from './common/logger/logger.module';
 import { parser } from './common/middleware/firebase-token-parser';
 import { PubSubModule } from './common/pubsub/pubsub.module';
 import { configuration } from './config';
 import { S3Module } from './app/s3/s3.module';
-import { HelloModule } from './app/user/user.module';
+import { UserModule } from './app/user/user.module';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
@@ -32,52 +29,19 @@ import { ScheduleModule } from '@nestjs/schedule';
           username: postgresConfig.dbUsername,
           password: postgresConfig.dbPassword,
           database: postgresConfig.dbName,
-          entities: ['dist/**/*.entity.{ts,js}'],
           migrations: ['dist/migrations/*.{ts,js}'],
           migrationsTableName: 'migrations',
           synchronize: !__prod__,
           logging: !__prod__,
+          autoLoadEntities: true,
         };
       },
       inject: [ConfigService],
     }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      fieldResolverEnhancers: ['guards'],
-      introspection: process.env.NODE_ENV !== 'production',
-      playground: process.env.NODE_ENV !== 'production',
-      subscriptions: {
-        'graphql-ws': {
-          onConnect: async (context: Context<any>) => {
-            const { connectionParams, extra } = context;
-            const authHeader = connectionParams.authorization;
-            // extract user information from token
-            const user = await parser(
-              authHeader,
-              connectionParams['x-mimic-user-id'],
-            );
-            extra.user = user;
-          },
-        },
-        'subscriptions-transport-ws': {
-          onConnect: async (connectionParams) => {
-            const authHeader = connectionParams.authorization;
-            // extract user information from token
-            const user = await parser(
-              authHeader,
-              connectionParams['x-mimic-user-id'],
-            );
-            return { extra: { user } };
-          },
-        },
-      },
-      autoSchemaFile: 'schema.gql',
-      installSubscriptionHandlers: true,
-    }),
     LoggerModule,
     PubSubModule,
     S3Module,
-    HelloModule,
+    UserModule,
   ],
 })
 export class AppModule {}
