6512a67fba6ad634998acea455882b1ab4813e8f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-12-15 03:16:45 +0530

    :fire: chore: removing graphql support

--- src/common/dto/pagination.ts
+++ src/common/dto/pagination.ts
@@ -1,13 +1,7 @@
-import { InputType, Field, Int } from '@nestjs/graphql';
-
-@InputType()
 export class PaginationDto {
-  @Field(() => Int, { nullable: true })
   offset?: number;
 
-  @Field(() => Int)
   limit: number;
 
-  @Field({ nullable: true })
   after?: string;
 }
