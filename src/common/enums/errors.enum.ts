b21ee18a9c3dc4fda54fcc0f2ff7b45f1a57dae6
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-12-17 18:05:31 +0530

    :zap: chore: Adding constants and enums

--- src/common/enums/errors.enum.ts
+++ src/common/enums/errors.enum.ts
@@ -1,3 +1,11 @@
 export enum CommonErrors {
-  FetchData = 'Unable fetch data',
+  FetchData = 'Unable to fetch data',
+  InvalidInput = 'Invalid input',
+}
+
+export enum UserErrors {
+  AccountDeleted = 'User is deleted',
+  InvalidEmail = 'Invalid email',
+  InvalidOtp = 'Invalid otp',
+  OtpLimitExceeded = 'Otp Limit exceeded',
 }
