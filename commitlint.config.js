d5ead213ab25a9253e36c03ccc95c49982363eb8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-12-18 19:38:44 +0530

    :wrench: chore: adding dependencies for commilint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['./node_modules/commitlint-config-gitmoji'],
   rules: {
-    'scope-enum': [2, 'always', ['global', 'globalkey']],
+    'scope-enum': [2, 'always', ['feat', 'fix', 'wip', 'chore', 'patch', 'build']],
   },
-};
+}
