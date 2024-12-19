14f8620525992bccfbfda22a370bf6e4038c83c9
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-12-20 02:19:22 +0530

    :hammer: chore: changing eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,7 @@ const eslintConfig = [
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       'unused-imports/no-unused-imports': 'error',
