eb9ef6ad7c3a81ae9296c4d4f7b1a34e2c0d7d81
Lunaticsatoshi <deysarkersudeep@gmail.com>
2024-12-20 13:31:19 +0530

    :zap: :chore: updating http-exceptions handling

--- src/common/middleware/http-exception.filter.ts
+++ src/common/middleware/http-exception.filter.ts
@@ -1,18 +1,28 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common'
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { IErrorResponse } from 'src/common/interfaces/error'
 import { LoggerService } from 'src/common/logger/logger.service'
 
-@Catch(HttpException)
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   constructor(private readonly loggerService: LoggerService) {}
-  catch(exception: HttpException, host: ArgumentsHost) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse<FastifyReply>()
     const request = ctx.getRequest<FastifyRequest>()
-    const status = exception.getStatus()
-    const error = exception.name
-    const message = exception.message
+    let status
+    let error
+    let message
+    if (exception instanceof HttpException) {
+      status = exception.getStatus()
+      error = exception.name
+      message = exception.message
+    } else {
+      status = HttpStatus.INTERNAL_SERVER_ERROR
+      error = 'Internal Server Error'
+      message = exception.message
+    }
 
     const responseJSON: IErrorResponse = {
       statusCode: status,
